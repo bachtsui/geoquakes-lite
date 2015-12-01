b17814207f839a332d9a77f306c6e8c6cfd88098
bachtsui <dbtsui2@gmail.com>
2015-12-01 15:57:14 -0800

    Earthquake Lab

--- base.js
+++ base.js
@@ -15,15 +15,7 @@ $(document).ready(function(){
   	method: "GET",
   	url: weekly_quakes_endpoint,
   	success: function (response){
-    // console.log(response.features[0].properties.title); //title
-    // console.log(response.features[0].geometry.coordinates[1]); //lat
-    // console.log(response.features[0].geometry.coordinates[0]); //long
-    // console.log(response.features[0].properties.time); //time
 		for (var i=0; i < response.features.length; i++){ 
-    		// console.log(response.features[i].properties.title); //title
-    		// console.log(response.features[i].geometry.coordinates[1]); //lat
-    		// console.log(response.features[i].geometry.coordinates[0]); //long
-    		// console.log(response.features[i].properties.time); //time
 		$("#info").append("Title: " + response.features[i].properties.title + " ");	
     	$("#info").append("Lat: " + response.features[i].geometry.coordinates[1] + " ");	
     	$("#info").append("Long: " + response.features[i].geometry.coordinates[0] + " ");	
@@ -35,16 +27,12 @@ $(document).ready(function(){
   		map: map,
   		title: response.features[i].properties.title
 		});
+
     	//Think about how you would do this with a forEach loop
+
 		}
 	}
 });
-
-// new google.maps.Marker({
-//   position: new google.maps.LatLng(37.4292,-122.1381),
-//   map: map,
-//   title: "title"
-// });
 
 });
 
